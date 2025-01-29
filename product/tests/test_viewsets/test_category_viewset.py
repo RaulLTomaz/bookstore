2aa4147dba6df12aa7b8440baff39a250c3848e1
Raul Lopes Tomaz <105323797+RaulLTomaz@users.noreply.github.com>
2025-01-29 18:33:05 -0300

    Rodado o comando make format-py

--- product/tests/test_viewsets/test_category_viewset.py
+++ product/tests/test_viewsets/test_category_viewset.py
@@ -12,11 +12,10 @@ class TestCategoryViewSet(APITestCase):
     client = APIClient()
 
     def setUp(self):
-        self.category = CategoryFactory(title='books')
+        self.category = CategoryFactory(title="books")
 
     def test_get_all_category(self):
-        response = self.client.get(
-            reverse("category-list", kwargs={"version": "v1"}))
+        response = self.client.get(reverse("category-list", kwargs={"version": "v1"}))
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
@@ -24,9 +23,7 @@ class TestCategoryViewSet(APITestCase):
         self.assertEqual(category_data["results"][0]["title"], self.category.title)
 
     def test_create_category(self):
-        data = json.dumps({
-            "title": "tecnology"
-        })
+        data = json.dumps({"title": "tecnology"})
 
         response = self.client.post(
             reverse("category-list", kwargs={"version": "v1"}),
